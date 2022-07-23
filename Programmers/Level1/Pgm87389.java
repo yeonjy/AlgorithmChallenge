038c3ccdbed1023fa988128cac0dbd68d24ce0bc
yeonjy <yeonjypaper@gmail.com>
2022-07-23 22:51:55 +0900

    나머지가 1이 되는 수 찾기

---
+++ Programmers/Level1/Pgm87389.java
@@ -0,0 +1,12 @@
+package pgm87389;
+
+class Solution {
+    public int solution(int n) {
+        int x;
+        for (x = 1; x <= n; x++) {
+            if (n % x == 1)
+                break;
+        }
+        return x;
+    }
+}
