e2fb354927805317c89e0608b47966a818f1e299
yeonjy <81320703+yeonjy@users.noreply.github.com>
2024-01-02 16:16:49 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1524 ms, Memory: 126300 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] a;
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        a = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(a);
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            int t = Integer.parseInt(st.nextToken());
+            sb.append(uppderIdx(t, n) - lowerIdx(t, n)).append(" ");
+        }
+        System.out.println(sb);
+    }
+
+    private static int lowerIdx(int target, int len) {
+        int st = 0;
+        int en = len;
+
+        while (st < en) {
+            int mid = (st + en) / 2;
+            if (a[mid] >= target) {
+                en = mid;
+            } else {
+                st = mid + 1;
+            }
+        }
+        return st;
+    }
+
+    private static int uppderIdx(int target, int len) {
+        int st = 0;
+        int en = len;
+
+        while (st < en) {
+            int mid = (st + en) / 2;
+            if (a[mid] > target) {
+                en = mid;
+            } else {
+                st = mid + 1;
+            }
+        }
+        return st;
+    }
+}
