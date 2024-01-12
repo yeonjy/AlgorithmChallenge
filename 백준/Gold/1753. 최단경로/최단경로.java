d2cde904598d56a81c65cb3bdaa1f0060533f560
yeonjy <81320703+yeonjy@users.noreply.github.com>
2024-01-13 03:47:22 +0900

    [Gold IV] Title: 최단경로, Time: 824 ms, Memory: 111144 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static class Node {
+        int v;
+        int cost;
+
+        public Node(int v, int cost) {
+            this.v = v;
+            this.cost = cost;
+        }
+    }
+
+    static ArrayList<Node>[] graph;
+    static boolean[] isVisited;
+    static int[] dist;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(br.readLine());
+
+        graph = new ArrayList[V + 1];
+        isVisited = new boolean[V + 1];
+        dist = new int[V + 1];
+
+        for (int i = 1; i <= V; i++) {
+            graph[i] = new ArrayList<>();
+            dist[i] = Integer.MAX_VALUE;
+        }
+
+
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            
+            graph[u].add(new Node(v, w));
+        }
+
+        dijkstra(K);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= V; i++) {
+            sb.append(dist[i] == Integer.MAX_VALUE ? "INF" : dist[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void dijkstra(int start) {
+        PriorityQueue<Node> q = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
+        q.add(new Node(start, 0));
+        dist[start] = 0;
+
+        while (!q.isEmpty()) {
+            Node now = q.poll();
+
+            if (!isVisited[now.v]) {
+                isVisited[now.v] = true;
+            }
+
+            for (Node next : graph[now.v]) {
+                if (!isVisited[next.v] && dist[next.v] > now.cost + next.cost) {
+                    dist[next.v] = now.cost + next.cost;
+                    q.add(new Node(next.v, dist[next.v]));
+                }
+            }
+        }
+    }
+}
