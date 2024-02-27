98ec3566b11affb8366e24825c92baadce06608a
yeonjy <81320703+yeonjy@users.noreply.github.com>
2024-02-28 01:11:49 +0900

    [Gold V] Title: 합분해, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final int MOD = 1000_000_000;
+    static int[][] dp;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        dp = new int[K + 1][N + 1];
+        Arrays.fill(dp[1], 1);
+        for (int i = 1; i <= K; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 2; i <= K; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                dp[i][j] %= MOD;
+            }
+        }
+
+        System.out.println(dp[K][N]);
+    }
+}
