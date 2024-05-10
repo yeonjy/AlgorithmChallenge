34024184d7019eedf4624a660e6265fdbf027da1
yeonjy <81320703+yeonjy@users.noreply.github.com>
2024-05-10 20:40:32 +0900

    [Gold IV] Title: 줄세우기, Time: 124 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] input = new int[N];
+        for (int i = 0; i < N; i++) {
+            input[i] = Integer.parseInt(br.readLine());
+        }
+        int[] dp = new int[N];
+        dp[0] = 1;
+
+        int ans = 0;
+        for (int i = 1; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (input[i] > input[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+        System.out.println(N - ans);
+    }
+}
