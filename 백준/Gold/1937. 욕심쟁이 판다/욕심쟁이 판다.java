295c14ac9665ae2d26e95b93e12ded95eca5ebc4
yeonjy <81320703+yeonjy@users.noreply.github.com>
2024-10-30 22:19:49 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 476 ms, Memory: 37176 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final int[] dx = new int[]{-1, 1, 0, 0};
+    static final int[] dy = new int[]{0, 0, -1, 1};
+    static int n;
+    static int[][] map;
+    static int[][] dp;
+    static int result;
+
+    public static void main(String[] args) throws IOException {
+        // 초기화
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+        dp = new int[n][n];
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (result < dfs(i, j)) {
+                    result = dp[i][j];
+                }
+            }
+        }
+        System.out.println(result);
+    }
+
+    static int dfs(int x, int y) {
+        if (dp[x][y] != 0) {
+            return dp[x][y];
+        }
+        dp[x][y] = 1;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (isValid(nx, ny) && map[nx][ny] > map[x][y]) {
+                dp[x][y] = Math.max(dp[x][y], dfs(nx, ny) + 1);
+            }
+        }
+        return dp[x][y];
+    }
+
+    static boolean isValid(int x, int y) {
+        return x >= 0 && y >= 0 && x < n && y < n;
+    }
+}
