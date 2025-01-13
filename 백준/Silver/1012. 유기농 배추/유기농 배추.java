5ba0d02d39806b7338db8f9c9e1c1bb2c1971dca
yeonjy <81320703+yeonjy@users.noreply.github.com>
2025-01-13 18:33:27 +0900

    [Silver II] Title: 유기농 배추, Time: 152 ms, Memory: 16400 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,78 +1,79 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb;
-    static boolean[][] map;
+    static final int[] dx = new int[]{-1, 1, 0, 0};
+    static final int[] dy = new int[]{0, 0, -1, 1};
+
+    static int N;
+    static int M;
     static boolean[][] isVisited;
-    static int[][] garbages;
-    static int cnt;
+    static boolean[][] map;
 
     public static void main(String[] args) throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        int test = Integer.parseInt(br.readLine());
-        sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < test; i++) {
-            input();
-        }
-
-
-
-        System.out.println(sb);
-    }
-
-    static void input() throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int width = Integer.parseInt(st.nextToken());
-        int height = Integer.parseInt(st.nextToken());
-        int num = Integer.parseInt(st.nextToken());
-
-        cnt = num;
-        map = new boolean[width + 2][height + 2];
-        isVisited = new boolean[width + 2][height + 2];
-        garbages = new int[num][2];
-
-        for (int i = 0; i < num; i++) {
+        for (int t = 0; t < T; t++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken()) + 1;
-            int y = Integer.parseInt(st.nextToken()) + 1;
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+            isVisited = new boolean[N][M];
+            map = new boolean[N][M];
 
-            map[x][y] = true;
-            garbages[i][0] = x;
-            garbages[i][1] = y;
-        }
-
-        for (int[] garbage : garbages) {
-            if (!isVisited[garbage[0]][garbage[1]]) {
-                bfs(garbage[0], garbage[1]);
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                map[x][y] = true;
             }
+
+            int result = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (map[i][j] && !isVisited[i][j]) {
+                        bfs(i, j);
+                        result++;
+                    }
+                }
+            }
+            sb.append(result + "\n");
         }
-        sb.append(cnt);
-        sb.append("\n");
+        System.out.print(sb);
     }
 
     static void bfs(int x, int y) {
-        isVisited[x][y] = true;
-        if (!isVisited[x + 1][y] && map[x + 1][y]) {
-            cnt--;
-            bfs(x + 1, y);
-        }
-        if (!isVisited[x - 1][y] && map[x - 1][y]) {
-            cnt--;
-            bfs(x - 1, y);
-        }
-        if (!isVisited[x][y + 1] && map[x][y + 1]) {
-            cnt--;
-            bfs(x, y + 1);
-        }
-        if (!isVisited[x][y - 1] && map[x][y - 1]) {
-            cnt--;
-            bfs(x, y - 1);
-        }
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
 
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            int nowX = now[0];
+            int nowY = now[1];
+            if (isVisited[nowX][nowY]) {
+                continue;
+            }
+            isVisited[nowX][nowY] = true;
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = nowX + dx[i];
+                int nextY = nowY + dy[i];
+
+                if (isValid(nextX, nextY) && map[nextX][nextY] && !isVisited[nextX][nextY]) {
+                    queue.add(new int[]{nextX, nextY});
+                }
+            }
+        }
+    }
+
+    static boolean isValid(int x, int y) {
+        return x >= 0 && y >= 0 && x < N && y < M;
     }
 }
