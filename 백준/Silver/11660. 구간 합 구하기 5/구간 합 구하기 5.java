b8f6f2510029052bad30dd9381d080b4aa91976d
yeonjy <81320703+yeonjy@users.noreply.github.com>
2025-01-13 22:32:54 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1044 ms, Memory: 122364 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -4,37 +4,48 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int A[][] = new int[N + 1][N + 1];
+
+        int[][] dp = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
-                A[i][j] = Integer.parseInt(st.nextToken());
+                int now = Integer.parseInt(st.nextToken());
+                dp[i][j] = dp[i][j - 1] + now;
             }
         }
 
-        int D[][] = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                // 구간 합 구하기
-                D[i][j] = D[i][j - 1] + D[i-1][j] - D[i-1][j-1] + A[i][j];
-            }
-        }
-
-        for (int i = 0; i < M; i++) {
+        for (int tc = 0; tc < M; tc++) {
+            int sum = 0;
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            //구간 합 배열로 질의에 답변하기
-            int result = D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1];
-            System.out.println(result);
+
+            if (x1 > x2) {
+                int tmp = x1;
+                x1 = x2;
+                x2 = tmp;
+            }
+            if (y1 > y2) {
+                int tmp = y1;
+                y1 = y2;
+                y2 = tmp;
+            }
+
+            for (int i = x1; i <= x2; i++) {
+                sum += dp[i][y2];
+                sum -= dp[i][y1 - 1];
+            }
+            sb.append(sum + "\n");
         }
+        System.out.println(sb);
     }
 }
