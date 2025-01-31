33838f9832c705258bb45b62f837c2fd3143c838
yeonjy <81320703+yeonjy@users.noreply.github.com>
2025-01-31 10:25:08 +0900

    [Gold III] Title: 내리막 길, Time: 332 ms, Memory: 36820 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -4,13 +4,15 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] dx = new int[]{-1, 1, 0, 0};
-    static int[] dy = new int[]{0, 0, -1, 1};
+    static int[] dx = new int[]{1, -1, 0, 0};
+    static int[] dy = new int[]{0, 0, 1, -1};
 
     static int M;
     static int N;
     static int[][] map;
     static int[][] dp;
+    static boolean[][] isVisited;
+    static int cnt;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,7 +21,7 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         map = new int[M][N];
         dp = new int[M][N];
-
+        isVisited = new boolean[M][N];
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
@@ -27,7 +29,6 @@ public class Main {
                 dp[i][j] = -1;
             }
         }
-
         System.out.println(dfs(0, 0));
     }
 
@@ -35,15 +36,17 @@ public class Main {
         if (x == M - 1 && y == N - 1) {
             return 1;
         }
-        if (dp[x][y] != -1) {
-            return dp[x][y];
+
+        if (dp[x][y] != -1) {  //첫 방문인지 확인하는 이유: 이미 방문한 곳이면 (x, y)에서
+            return dp[x][y];   //도착지까지 가는 경로 탐색한 곳이기 때문
         }
         dp[x][y] = 0;
 
         for (int i = 0; i < 4; i++) {
             int nextX = x + dx[i];
             int nextY = y + dy[i];
-            if (isValid(nextX, nextY) && map[x][y] > map[nextX][nextY]) {
+
+            if (isValid(nextX, nextY) && (map[x][y] > map[nextX][nextY])) {
                 dp[x][y] += dfs(nextX, nextY);
             }
         }
@@ -51,6 +54,6 @@ public class Main {
     }
 
     static boolean isValid(int x, int y) {
-        return x >= 0 && y >= 0 && x < M && y < N;
+        return x >= 0 && x < M && y >= 0 && y < N;
     }
 }
