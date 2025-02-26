2bd755ee4dfe02a03e559ec9afe7b18cc0cb4294
yeonjy <81320703+yeonjy@users.noreply.github.com>
2025-02-26 14:36:40 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 276 ms, Memory: 23876 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -4,48 +4,24 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static final int[] dx = new int[]{0, -1, -1};
-    static final int[] dy = new int[]{-1, -1, 0};
-    static int n, m;
-    static int[][] map;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
-        boolean oneExist = false;
-        int res = 0;
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[n + 1][m + 1];
+        int max = 0;
 
-        for (int i = 0; i < n; i++) {
-            String[] nums = br.readLine().split("");
-            for (int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(nums[j]);
-                if (!oneExist && map[i][j] == 1) {
-                    res = 1;
+        for (int i = 1; i <= n; i++) {
+            String line = br.readLine();
+            for (int j = 1; j <= m; j++) {
+                if (line.charAt(j - 1) == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    max = Math.max(max, dp[i][j]);
                 }
             }
         }
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (map[i][j] == 1) {
-                    map[i][j] = getLen(i, j) + 1;
-                    res = Math.max(res, map[i][j]);
-                }
-            }
-        }
-        System.out.println(res * res);
-
-    }
-
-    static int getLen(int x, int y) {
-        for (int k = 0; k < 3; k++) {
-            if (map[x + dx[k]][y + dy[k]] < 1) {
-                return 0;
-            }
-        }
-        return Math.min(map[x][y - 1], Math.min(map[x - 1][y - 1], map[x - 1][y]));
+        System.out.println(max * max);
     }
 }
